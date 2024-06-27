cf7470ae39ad4ed913722b3d7fb4f7d1dec92348
Shubham Chauhan <shubham.chauhan860@gmail.com>
2024-06-27 11:51:57 +0530

    fix(output): new output ques are added

--- jsImplementations/curryImplementation.js
+++ jsImplementations/curryImplementation.js
@@ -2,14 +2,14 @@
 
 function convertCurry (fn) {
     return function curried(...args) {
-        if(args.length >= fn.length) {
-            return fn(...args);
-        } else {
-            return function(...next) {
-                console.log('else', args, next)
-                return curried(...args, ...next);
+            if (args.length >= fn.length) {
+                return fn(...args.slice(0, fn.length));
+            } else {
+                return function(...next) {
+                    console.log('else', args, next)
+                    return curried(...args, ...next);
+                }
             }
-        }
     }
 }
 
@@ -18,7 +18,7 @@ const sum = (a,b,c,d) => {
 };
 
 const curryFunction = convertCurry(sum);
-console.log(curryFunction(1)(2)(3)(4))
+console.log(curryFunction(1)(2)(3)(4)(5)(6)) // creating error we need to fix it for n params
 
 // Running
 // else [ 1 ] [ 2 ]
