3798ab1ee5a1e6c9c1e1db2b96012afeb85431a4
Shubham Chauhan <shubham.chauhan860@gmail.com>
2024-07-08 02:25:12 +0530

    fix(js): new chaining questions are added

--- jsImplementations/chainingQuestion.js
+++ jsImplementations/chainingQuestion.js
@@ -27,7 +27,7 @@ function Calculator() {
     this.value = 0;
 }
 
-Calculator.prototype.add = function(number) {
+Calculator.prototype.add = function (number) {
     this.value += number;
     return this;
 }
@@ -58,24 +58,119 @@ console.log('2', calci.add(10).multiply(5).subtract(10).getResult()); //40
 
 class CalculatorClass {
     constructor() {
-      this.value = 0;
+        this.value = 0;
     }
     add(number) {
-      this.value += number;
-      return this;
+        this.value += number;
+        return this;
     }
     multiply(number) {
-      this.value *= number;
-      return this;
+        this.value *= number;
+        return this;
     }
     subtract(number) {
-      this.value -= number;
-      return this;
+        this.value -= number;
+        return this;
     }
     getResult() {
-      return this.value;
+        return this.value;
     }
-  }
-  
-  const calco = new CalculatorClass();
-  console.log('3', calco.add(10).multiply(5).subtract(10).getResult()); // 40
+}
+
+const calco = new CalculatorClass();
+console.log('3', calco.add(10).multiply(5).subtract(10).getResult()); // 40
+
+
+// -------------------------------------------------------------------------// 
+
+function math() {
+    return {
+        count: 0,
+        add(number) {
+            this.count += number;
+            return this;
+        },
+        sub(number) {
+            this.count -= number;
+            return this;
+        },
+        mul(number) {
+            this.count *= number;
+            return this;
+        },
+        value() {
+            return this.count;
+        }
+    }
+}
+
+console.log('4', math().add(3).sub(2).mul(5).sub(3).value());
+
+
+// -------------------------------------------------------------------------// 
+
+function computeAmount() {
+    return {
+        total: 0,
+        Lacs(count) {
+            this.total += count * 100000;
+            return this;
+        },
+        crores(count) {
+            this.total += count * 10000000;
+            return this;
+        },
+        thousand(count) {
+            this.total += count * 1000;
+            return this;
+        },
+        value() {
+            return this.total;
+        }
+    }
+}
+
+
+console.log('5', computeAmount().Lacs(2).crores(1).thousand(5).Lacs(3).value())
+
+// -------------------------------------------------------------------------// 
+
+function num() {
+    let value = null;
+    let minVal = null;
+    let maxVal = null;
+
+    return {
+        min: function (min) {
+            minVal = min;
+            return this;
+        },
+        max: function (max) {
+            maxVal = max;
+            return this;
+        },
+        validate: function (input) {
+            if (isNaN(input)) {
+                throw new Error("Input is not a number");
+            }
+
+            value = parseFloat(input);
+
+            if (minVal !== null && value < 2) {
+                throw new Error(`Value should be greater than or equal to 2`);
+            }
+
+            if (maxVal !== null && value > 10) {
+                throw new Error(`Value should be less than or equal to 10`);
+            }
+
+            return value;
+        }
+    };
+}
+
+let x = num().min(2).max(10)
+x.validate(1) // should throw error should be less than 2
+x.validate(11) // should throw error should not be greater than 10
+console.log(x.validate(5)) // should return 5
+console.log(x.validate('5')) // should return 5
